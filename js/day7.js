87feaa0d9699c452cb11b8c2b32ea62afc21e535
viskjha <vishaljha511@gmail.com>
2019-06-22 22:57:30 +0530

    Add jQuery

--- js/day7.js
+++ js/day7.js
@@ -14,25 +14,39 @@
 
 // Form validation Using JQuery
 
-$("form").submit(function(event)
-{
-    event.preventDefault();
-});
+$(document).ready(function () {
+    $("form").submit(function (event) {
+        console.log("Hi")
+        event.preventDefault();
+    });
 
-$("#pass").bind('blur', function()
-{
-    let pass = $("#pass").val();
-    let pass2 = $("#pass2").val();
-    if(pass =="")
-    {
-        $("#passErr").html("Empty");
-        $("#passErr").css("color", "red");
-        $("#passErr").show();
-    }
+    $("#pass").bind('blur', function () {
+        let pass = $("#pass").val();
+        if (pass == "") {
+            $("#passErr").html("Empty");
+            $("#passErr").css("color", "red");
+            $("#passErr").css("text-decoration", "underline");
+            $("#passErr").show();
+        } else {
+            $("#passErr").html("Correct");
+            $("#passErr").css("color", "blue");
+        }
+    });
 
-    else
-    {
-        $("#passErr").html("correct");
-        $("#passErr").css("color","blue"); 
-    }
+    $("#pass2").bind('blur', function () {
+        let pass = $("#pass").val();
+        let pass2 = $("#pass2").val();
+        if (pass2 == "") {
+            $("#pass2Err").html("Empty");
+            $("#pass2Err").css("color", "red");
+            $("#pass2Err").show();
+        } else if (pass !== pass2) {
+            $("#pass2Err").html("Not matching");
+            $("#pass2Err").css("color", "red");
+            $("#pass2Err").show();
+        } else {
+            $("#pass2Err").html("Correct");
+            $("#pass2Err").css("color", "blue");
+        }
+    });
 });
