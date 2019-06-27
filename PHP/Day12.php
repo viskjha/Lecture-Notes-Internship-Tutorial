b16d67aba7964be4acf719ff880e851ce8d4a7af
viskjha <vishaljha511@gmail.com>
2019-06-27 16:26:53 +0530

    validation and display data

---
+++ PHP/Day12.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Day12</title>
+</head>
+<body>
+ 
+ <!-- validation and fill value -->
+ <?php
+ $fnameErr = $lnameErr = $emailErr ="";
+  $fname = $lname= $email ="";
+     if(!empty($_POST))
+     {
+         if(empty($_POST['fname']))
+         {
+             echo "First Name must be filled<br>";
+         }
+         else {
+             $fname = $_POST['fname'];
+         }
+
+         if(empty($_POST['lname']))
+         {
+             echo "Last Name must be filled<br>";
+         }
+         else {
+             $lname = $_POST['lname'];
+         }
+
+         if(empty($_POST['email']))
+         {
+             echo "Email must be filled<br>";
+         }
+         else {
+             $email = $_POST['email'];
+         }
+     }
+ ?>
+
+ <!-- Form handling in PHP -->
+<form action="" method="POST">
+First Name:<input type="text" name="fname" value=<?php echo $fname; ?>> <br>
+<p><?php echo $fnameErr; ?></p>
+
+Last Name:<input type="text" name="lname" value="<?php echo $lname; ?>"><br>
+<p><?php echo $lnameErr; ?></p>
+
+Email:<input type="email" name="email" value=<?php echo $email; ?>><br>
+<p><?php echo $emailErr; ?></p>
+
+<input type="submit" name="submit" value="submit">
+</form>
+
+
+<!-- Display Data Of Form -->
+<?php
+     if(!empty($_POST))
+     {
+         echo "First Name: ".$_POST['fname']. "<br>";
+         echo "Last Name: ".$_POST['lname']. "<br>";
+         echo "Email: ".$_POST['email']. "<br><br>";
+     }
+ ?>
+
+    
+</body>
+</html>
